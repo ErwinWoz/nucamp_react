5fa815a237981cdc5af3cb89063918250200bcb3
ErwinWoz <eerwoz@gmail.com>
2020-06-04 22:38:12 -0700

    React Router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return (
@@ -8,10 +9,10 @@ function Footer(props) {
                     <div className="col-4 col-sm-2 offset-1">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="aboutus.html">About</a></li>
-                            <li><a href="#">Sites</a></li>
-                            <li><a href="contactus.html">Contact</a></li>
+                            <li><Link to='/home'>Home</Link></li>
+                            <li><Link to='/directory'>Directory</Link></li>
+                            <li><Link to='/aboutus'>About</Link></li>
+                            <li><Link to='/contactus'>Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-6 col-sm-5 text-center">
@@ -22,7 +23,7 @@ function Footer(props) {
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><span className="fa fa-youtube"></span></a>
                     </div>
                     <div className="col-sm-4 text-center">
-                        <a role="button" class="btn btn-link" href="tel:+12065551234"><span className="fa fa-phone fa-lg text-primary"/> 1-206-555-1234 </a><br />
+                        <a role="button" className="btn btn-link" href="tel:+12065551234"><span className="fa fa-phone fa-lg text-primary"/> 1-206-555-1234 </a><br />
                         <a role="button" className="btn btn-link" href="mailto:campsites@nucamp.co"><span className="fa fa-envelope fa-lg text-primary" /> campsites@nucamp.co </a>
                     </div>
                 </div>
